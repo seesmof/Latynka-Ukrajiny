875d5857b10b49504d7e1fce0e76e44e19c774a0
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-27 16:41:00 +0300

    feat: rewrite converter in js ✝️💖 praise Jesus Christ our Holy Lord GOD Almighty

--- test/test_manual.py
+++ test/test_manual.py
@@ -1,17 +1,3 @@
 from latynka import convert, Romanizer
 
-text: str = (
-    "Слава Ісусу Христу Святому Господу БОГУ Одвічному Всемогутньому Всевишньому навіки Вічні АМІНЬ!"
-)
-print(convert(text))
-
-text: str = (
-    "ґудзик ґанок гліб глобус м'яч п'ять сім'я їхати під'їзд приїхали наїздилися Йошуа Єшуа Мошей Рут Моав Адонай"
-)
-print(Romanizer(text).romanize())
-
-input_text: str = "СЛАВА НАВІКИ БОГУ ЄДИНОМУ ІСУСУ ХРИСТУ СВЯТОМУ ГОСПОДУ"
-converter = Romanizer()
-converter.set_text(input_text)
-output_text: str = converter.romanize()
-print(output_text)
+print(convert("п'ять"))
