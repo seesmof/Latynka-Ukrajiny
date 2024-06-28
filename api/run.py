b5a630a20b49d849fe17ad9501460a956df962f2
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-28 21:53:31 +0300

    feat: api add queries handler ✝️💓 praise Jesus Christ our Holy Lord GOD Almighty

--- api/run.py
+++ api/run.py
@@ -5,4 +5,23 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "Hallelujah thank YOU Jesus Christ our Holy Lord GOD Almighty"
+    return "Hallelujah thank YOU Jesus Christ our Holy Lord GOD Almighty ✝️💖 enter a text to get the response"
+
+
+@app.get("/{text}")
+async def convert(text: str):
+    """
+    Takes in a Ukrainian text in cyrillic and converts it to latin (romanizes, makes into latynka)
+
+    < text: str
+        Ukrainian text in cyrillic
+    > str
+        Ukrainian text in latin
+    """
+
+    # TODO install latynka library and use it here. for now, just title case
+    def convert(text: str) -> str:
+        return text.title()
+
+    print(text)
+    return convert(text)
